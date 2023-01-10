52a3cd7b92fe1d1b7b878b66cfad3834e38db8fa
DanaKononova <dana.kononova@mail.ru>
2023-01-11 00:09:06 +0300

    commit homework

--- app/src/main/java/com/example/projectwithpictures/MainActivity.kt
+++ app/src/main/java/com/example/projectwithpictures/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         val button = findViewById<Button>(R.id.button1)
         val imageView = findViewById<ImageView>(R.id.iv_photo)
-        val pictures = listOf (
+        val pictures = listOf(
             "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
             "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg",
             "https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712__480.jpg",
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             "https://cdn.pixabay.com/photo/2018/01/31/07/36/secret-3120483__480.jpg",
             "https://cdn.pixabay.com/photo/2015/02/14/19/46/winter-landscape-636634__480.jpg",
             "https://cdn.pixabay.com/photo/2017/09/18/15/38/moon-2762111_1280.jpg"
-            )
+        )
         var iterator = 0;
         val LIMIT = 10
 
